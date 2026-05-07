0459a87524567b10bdfdc81bb533ff7895b55ed1
Eve <benlovesyellow@hotmail.com>
2026-05-06 23:55:57 -0400

    feat: add supabase config and shared styles

---
+++ config.js
@@ -0,0 +1,3 @@
+var SUPABASE_URL = 'https://utqiyrpmohnribkkirht.supabase.co'
+var SUPABASE_ANON_KEY = 'sb_publishable_ifAoOWAhwKm-eBHcdkwv5A_MSWlIyFh'
+var db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
